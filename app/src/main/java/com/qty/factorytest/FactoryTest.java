a66fb976987826c743267f2f3ee941c42bd94c92
xiaotuan <583168425@qq.com>
2016-09-06 05:10:37 +0800

    init Factory Test Project

---
+++ app/src/main/java/com/qty/factorytest/FactoryTest.java
@@ -0,0 +1,13 @@
+package com.qty.factorytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FactoryTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factory_test);
+    }
+}
