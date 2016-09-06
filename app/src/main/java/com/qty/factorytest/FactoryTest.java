d972db93a6ce6387a2cb65de666533893dada168
qtunye <qin_ty@chinaxuhu.com>
2016-09-06 09:44:26 +0800

    first add

--- app/src/main/java/com/qty/factorytest/FactoryTest.java
+++ app/src/main/java/com/qty/factorytest/FactoryTest.java
@@ -1,9 +1,10 @@
 package com.qty.factorytest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FactoryTest extends AppCompatActivity {
+public class FactoryTest extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
